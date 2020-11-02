2c5ca5babdc290d3e5bdefa44cd24f93f7a6225c
Björn Tirsén <bjorntirsen@gmail.com>
2020-11-02 08:41:36 +0100

    Renamed app to docs

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var sass        = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
+    return gulp.src("docs/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("app/css"))
+        .pipe(gulp.dest("docs/css"))
         .pipe(browserSync.stream());
 });
 
@@ -14,11 +14,11 @@ gulp.task('sass', function() {
 gulp.task('serve', gulp.series('sass', function() {
 
     browserSync.init({
-        server: "./app/"
+        server: "./docs/"
     });
 
-    gulp.watch("app/scss/*.scss", gulp.series('sass'));
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("docs/scss/*.scss", gulp.series('sass'));
+    gulp.watch("docs/*.html").on('change', browserSync.reload);
 }));
 
 gulp.task('default', gulp.series('serve'));
