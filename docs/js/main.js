4115d10b674e0f0ee0ac919c42bc8d6d52a7581f
Björn Tirsén <bjorntirsen@gmail.com>
2020-11-02 13:56:09 +0100

    Trying out some stuff

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", e => {
     //Initiate interface and swup library
     /* const interface = new Interface(); */
-    const swup = new Swup();
+    /* const swup = new Swup(); */
 })
