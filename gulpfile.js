ee421cd49f765a138e236d0e6835e447e201b704
Björn Tirsén <bjorntirsen@gmail.com>
2020-11-14 15:43:29 +0100

    Testing a purged CSS on portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var purgecss = require('gulp-purgecss');
+
+// Purge that nasty Bootstrap CSS
+gulp.task('purgecss', () => {
+    return gulp.src('docs/css/*.css')
+        .pipe(purgecss({
+            content: ['docs/*.html']
+        }))
+        .pipe(gulp.dest('docs/purecss'))
+});
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
